186419bd5f26129c517ca04b666c3ad1d59ca251
Robin Joseph <robinjoseph11998@gmail.com>
2024-04-08 12:53:25 +0530

    Result model setuped

---
+++ utils/model.go
@@ -0,0 +1,15 @@
+package utils
+
+type Result struct {
+	Month       string `json:"month"`
+	Num         int    `json:"num"`
+	Link        string `json:"link"`
+	Year        string `json:"year"`
+	News        string `json:"news"`
+	SafeTitle   string `json:"safe_title"`
+	Trasnscript string `json:"transcript"`
+	Alt         string `json:"slt"`
+	Img         string `json:"img"`
+	Title       string `json:"title"`
+	Day         string `json:"day"`
+}
